de5550b03f7fc7ea55e72c364b1c019c0660cf70
Leslie <rleslie1015@gmail.com>
2019-06-14 10:51:59 -0500

    File structure

---
+++ todos/src/main/java/com/lambda/todos/services/RoleService.java
@@ -0,0 +1,16 @@
+package com.lambda.todos.services;
+
+import com.lambda.todos.model.Role;
+
+import java.util.List;
+
+public interface RoleService
+{
+    List<Role> findAll();
+
+    Role findRoleById(long id);
+
+    void delete(long id);
+
+    Role save(Role role);
+}
